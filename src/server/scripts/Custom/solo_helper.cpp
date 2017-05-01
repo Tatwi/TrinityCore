2d5a8787d3f1d48b12e98fce3b7ec3cc6d77be34
R. Bassett Jr. (Tatwi) <rbassettjr@tpot.ca>
2017-05-01 15:02:54 -0400

    - Fixed segfault - Changed some spells - Fixed some log issues - Added chat message on NPC dismiss - Tests OK, perhaps normal NPCs are too strong atm

--- src/server/scripts/Custom/solo_helper.cpp
+++ src/server/scripts/Custom/solo_helper.cpp
@@ -13,20 +13,21 @@ enum Spells
     SPELL_BUFF_A = 20135,       // Redoubt
     SPELL_BUFF_B = 20261,		// Divine Intelect
     SPELL_BUFF_C = 20217,		// Blessing of Kings
-    SPELL_TAUNT = 355,          // Taunt
+    SPELL_BUFF_D = 10060,       // Power Infusion
     SPELL_DAMAGE_A = 53385,     // Divine Storm
     SPELL_DAMAGE_B = 53595,     // Hammer of the Righteous
     SPELL_DAMAGE_C = 36835,     // Warstomp
-    SPELL_FEAR = 16508,     	// Intimidating Roar
     SPELL_ELITE_A = 41367,      // Divine Shield
     SPELL_ELITE_B = 59159,		// Thunderstorm
+    SPELL_ELITE_C = 48171,		// Resurection
+    THREAT_FACTOR = 10000,      // Taunt per swing
 };
 
 class solo_helper: CreatureScript
 {
 public:
     solo_helper() : CreatureScript("soloHelper") {}
- 
+    
 	struct solo_helperAI : public ScriptedAI
 	{
 		solo_helperAI(Creature* creature) : ScriptedAI(creature) {}
@@ -34,8 +35,9 @@ public:
 		uint32 cooldownDuration = 100; // When on cooldown, count up to this number every server tick
 		uint32 cooldownTimer = 0;
 		bool onCoolDown = false;
+		Unit* player = NULL;
 		
-		void Reset()
+		void Reset() // On spawn, evade, end of combat
 		{
 			DoCast(me, SPELL_PROTECTION_A);
 			DoCast(me, SPELL_BUFF_B);
@@ -43,32 +45,31 @@ public:
 
 		}
 		
-		void EnterCombat(Unit* who)
+		void impressiveTaunt()
 		{
 			Unit* victim = me->GetVictim();
-			DoCast(victim, SPELL_TAUNT);
+			me->AddThreat(victim, (float)THREAT_FACTOR);
+		}
+		
+		void EnterCombat(Unit* who)
+		{
+			impressiveTaunt();
 		}
 		
 		void UpdateAI(const uint32 uiDiff)
 		{
-			// If not in combat, follow player. frand(-3.0f, 3.0f) is help prevent multiple helpers from overlapping.
+			player = me->SelectNearestPlayer(5000.0f);
+			
+			// Not in combat:
 			if (!me->GetVictim())
 			{
-				Unit* player = me->SelectNearestPlayer(5000.0f);
-				
-				// Player dead or too far away?
-				if (player == NULL || player->isDead())
+				if (player != NULL)
 				{
-					me->CleanupsBeforeDelete();
-					delete me;
+					if (!me->isMoving())
+						me->SetHomePosition(player->GetPositionX() + frand(-3.0f, 3.0f), player->GetPositionY() + frand(-3.0f, 3.0f), player->GetPositionZ(), me->GetFollowAngle());
 					
-					return;
+					me->GetMotionMaster()->MoveFollow(player, frand(-3.0f, 3.0f), me->GetFollowAngle());
 				}
-				
-				if (!me->isMoving())
-					me->SetHomePosition(player->GetPositionX() + frand(-3.0f, 3.0f), player->GetPositionY() + frand(-3.0f, 3.0f), player->GetPositionZ(), me->GetFollowAngle());
-				
-				me->GetMotionMaster()->MoveFollow(player, frand(-3.0f, 3.0f), me->GetFollowAngle());	
 			}
 			
 			if (!UpdateVictim())
@@ -92,12 +93,11 @@ public:
 					// Elite Only Attack
 					DoCast(victim, SPELL_ELITE_A);
 					DoCast(victim, SPELL_ELITE_B);
-					DoCast(me, SPELL_PROTECTION_B);
 					cooldownTimer = 20;
 				} else if (rng > 95){
 					DoCast(me, SPELL_PROTECTION_B);
 				} else if (rng > 90){
-					DoCast(victim, SPELL_TAUNT);
+					DoCast(me, SPELL_PROTECTION_A);
 				} else if (rng > 70){
 					DoCast(victim, SPELL_DAMAGE_A);
 				} else if (rng > 45){
@@ -105,7 +105,7 @@ public:
 				} else if (rng > 20){
 					DoCast(victim, SPELL_DAMAGE_C);
 				} else if (rng > 15){
-					DoCast(victim, SPELL_FEAR); 
+					DoCast(victim, SPELL_BUFF_D); 
 				} else if (rng > 0){
 					DoCast(me, SPELL_HEAL);
 				}
@@ -113,8 +113,9 @@ public:
 			} 
 			
 			DoMeleeAttackIfReady();
+			impressiveTaunt();
 
-			cooldownTimer++; // Approx every 55ms
+			cooldownTimer++; // Server "tick" approx every 55ms
 			
 			if (cooldownTimer > cooldownDuration){
 				onCoolDown = false;
@@ -129,11 +130,12 @@ public:
 	
 	bool OnGossipSelect(Player* player, Creature* creature, uint32 uiSender, uint32 uiAction)
     {
-		// delete creature
-		creature->CleanupsBeforeDelete();
-        delete creature;
-        
+		player->PlayerTalkClass->ClearMenus();
 		player->PlayerTalkClass->SendCloseGossip();
+		
+		creature->Say("Walking down the street! Looking at my feet! Watching the leaves, fall off of the trees!", LANG_UNIVERSAL);
+		creature->CleanupsBeforeDelete();
+		delete creature;
 		
 		return true;
     }
