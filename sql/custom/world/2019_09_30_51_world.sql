d0fb8a72b1e8269b431c0f5e3897b5636a8ad53d
R. Bassett Jr. (Tatwi) <alsopeachy@gmail.com>
2019-09-30 09:50:58 -0400

    Added Vendor: Stone & Ore

--- sql/custom/world/2019_09_30_51_world.sql
+++ sql/custom/world/2019_09_30_51_world.sql
@@ -6,7 +6,9 @@ DELETE FROM creature WHERE guid BETWEEN 15000210 AND 15000220;
 INSERT INTO `creature` (`guid`,`id`,`map`,`spawnMask`,`phaseMask`,`modelid`,`equipment_id`,`position_x`,`position_y`,`position_z`,`orientation`,`spawntimesecs`,`spawndist`,`currentwaypoint`,`curhealth`,`curmana`,`MovementType`) 
 VALUES
 -- Leather
-('15000210','500054','0','1','1','0','0','-2092.5','-1990.04','0.3884','3.371439','300','0','0','24009944','0','0');
+('15000210','500054','0','1','1','0','0','-2092.5','-1990.04','0.3884','3.371439','300','0','0','24009944','0','0'),
+-- Metal & Stone
+('15000211','500060','0','1','1','0','0','-2091.96','-1992.37','0.3674','3.371439','300','0','0','24009944','0','0');
 
 -- Adjust prices of items
 -- Leather
@@ -17,6 +19,10 @@ UPDATE `item_template` SET `BuyPrice`='15000' WHERE `entry`='29547'; -- Wind Sca
 UPDATE `item_template` SET `BuyPrice`='15000' WHERE `entry`='29539'; -- Cobra Scales 1.5g
 UPDATE `item_template` SET `BuyPrice`='12500' WHERE `entry`='25700'; -- Fel Scales 1.25g
 UPDATE `item_template` SET `BuyPrice`='20000' WHERE `entry`='25699'; -- Crystal Infused Leather 2g
+-- Metal & Stone
+UPDATE `item_template` SET `BuyPrice`='20000' WHERE `entry`='36915'; -- Froststeel Bar 2g
+UPDATE `item_template` SET `BuyPrice`='30000' WHERE `entry`='36914'; -- Azurite Bar 3g
 
 -- Modify character appearances
 UPDATE `creature_template` SET `modelid1`='3277', `scale`='1' WHERE `entry`='500054'; -- Leather
+UPDATE `creature_template` SET `modelid1`='4637', `scale`='1' WHERE `entry`='500060'; -- Metal & Stone
